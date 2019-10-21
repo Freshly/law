ca3dfa051c86e36dc9faf63787614e94fdb43cdb
Eric Garside <garside@gmail.com>
2019-10-21 10:46:02 -0400

    PermissionBase
    Add a root object for representing Permissions.

--- spec/support/shared_context/with_an_example_role.rb
+++ spec/support/shared_context/with_an_example_role.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.shared_context "with an example role" do
-  subject(:example_role) { example_role_class.new(source) }
+  subject(:example_role) { example_role_class.new }
 
   let(:example_role_class) { Class.new(Law::RoleBase) }
-
-  let(:source) { double }
 
   let(:root_name) { Faker::Internet.domain_word.capitalize }
   let(:example_role_name) { "#{root_name}Role" }
