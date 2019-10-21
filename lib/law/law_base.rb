43b5a1ace0b661e33dd83e8c3c659f2f02db751b
Eric Garside <garside@gmail.com>
2019-10-21 19:40:11 -0400

    Laws::Regulations
    Allow Laws to enforce Regulations.

--- lib/law/law_base.rb
+++ lib/law/law_base.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require_relative "laws/regulations"
+
 # A **Law** is a collection of **Regulations**.
 module Law
   class LawBase < Spicerack::RootObject
+    include Law::Laws::Regulations
   end
 end
