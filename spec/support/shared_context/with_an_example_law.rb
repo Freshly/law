c67b64a38e9a80a7650ccaff6c3bb35828c19668
Eric Garside <garside@gmail.com>
2019-10-28 14:32:17 -0400

    Law::Base
    - Establishing an object for defining responsibilities

---
+++ spec/support/shared_context/with_an_example_law.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+RSpec.shared_context "with an example law" do
+  subject(:example_law) { example_law_class.new }
+
+  let(:example_law_class) { Class.new(Law::Base) }
+
+  let(:root_name) { Faker::Internet.domain_word.capitalize }
+  let(:example_law_name) { "#{root_name}Law" }
+
+  before { stub_const(example_law_name, example_law_class) }
+end
