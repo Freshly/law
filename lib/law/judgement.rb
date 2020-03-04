c588f44ce370640c6b75740a96eea14cccddb4d2
Eric Garside <garside@gmail.com>
2020-03-03 23:20:22 -0500

    Require Full Compliance

--- lib/law/judgement.rb
+++ lib/law/judgement.rb
@@ -39,21 +39,30 @@ module Law
     end
 
     def judge!
+      raise AlreadyJudgedError if adjudicated?
+
       if statute&.unregulated?
         @applied_regulations = [ nil ]
         return true
       end
 
-      raise InjunctionError if applicable_regulations.blank?
-      raise ComplianceError unless compliant?
-      raise AlreadyJudgedError if adjudicated?
-
-      @applied_regulations = applicable_regulations.map { |regulation| regulation.new(petition: petition) }
-      @violations = applied_regulations.reject(&:valid?)
-
-      raise NotAuthorizedError unless authorized?
+      ensure_jurisdiction
+      reckon!
 
       true
+    end
+
+    private
+
+    def ensure_jurisdiction
+      raise InjunctionError if applicable_regulations.blank?
+      raise ComplianceError unless compliant?
+    end
+
+    def reckon!
+      @applied_regulations = applicable_regulations.map { |regulation| regulation.new(petition: petition) }
+      @violations = applied_regulations.reject(&:valid?)
+      raise NotAuthorizedError unless authorized?
     end
   end
 end
