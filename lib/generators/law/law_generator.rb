f7080bbb5bf185fdacc7147f68a398b3a827e22c
Brandon Trumpold <brandon.trumpold@freshly.com>
2020-04-12 21:35:42 -0400

    wip v2 - trying hooks with default false

--- lib/generators/law/law_generator.rb
+++ lib/generators/law/law_generator.rb
@@ -3,12 +3,15 @@
 module Law
   module Generators
     class LawGenerator < Rails::Generators::NamedBase
-      class_option :statute, type: :boolean, default: true
-      class_option :regulation, type: :boolean, default: true
+      class_option :statute, type: :boolean, default: false
+      class_option :regulation, type: :boolean, default: false
 
       source_root File.expand_path("templates", __dir__)
 
+      hook_for :statute, as: "law:statute"
+      hook_for :regulation, as: "law:regulation"
       hook_for :test_framework
+
 
       def create_application_flow
         template "law.rb.erb", File.join("app/laws/", class_path, "#{file_name}_law.rb")
