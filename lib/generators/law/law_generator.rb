906cbb99559fbd55a9f74c186d3fc25690b425b2
Brandon Trumpold <brandon.trumpold@freshly.com>
2020-04-12 20:42:40 -0400

    added law/statute generators - ready to test on cepro

---
+++ lib/generators/law/law_generator.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Law
+  module Generators
+    class LawGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path("templates", __dir__)
+
+      hook_for :test_framework
+
+      def create_application_flow
+        template "law.rb.erb", File.join("app/laws/", class_path, "#{file_name}_law.rb")
+      end
+    end
+  end
+end
